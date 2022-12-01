047f6759522167eb9e7040e738356565f5977ab0
Scott Parkhill <scottparkhill@culturefoundrystudios.com>
2022-12-01 10:42:14 -0500

    Adds logic for handling the NTSC standard output

--- ultimate_tag.asm
+++ ultimate_tag.asm
@@ -11,10 +11,7 @@
     seg.u Variables         ; Defines uninitialized segment named Variables.
     org $80                 ; Set origin of segment at beginning of RAM.
 
-; This is just here so I remember how to do variable declaration.
-; DataByte .byte ; declare 8-bit value
-; DataWord .word ; declare 16-bit value
-; DataArray ds 20 ; reserve 20 bytes for array
+BGColour .byte              ; Sets the background colour variable.
 
 ; Creates segment for the main program.
     seg Program             ; Defines the initialized code segment of the program.
@@ -24,10 +21,79 @@
 Initialize                  ; Defines the Initialize subroutine.
     CLEAN_START             ; Calls the CLEAN_START macro from macro.h.
 
-    JMP Initialize          ; Loop.
+    LDA #$9F                ; Loads sky blue into the accumulator.
+    STA BGColour            ; Stores the colour into the BGColour variable
+    STA COLUBK              ; Sets the background colour register in the TIA.
 
-    org $FFFC               ; Fills ROM out to exactly 4K in size and tell it where to start the program.
-    .word Initialize        ; Reset pointer at $FFFC
-    .word Initialize        ; Interrupt pointer at $FFFE (unused but apparently a good idea).
+    LDA #2                  ; Sets the binary value #%0000_0010, which will turn on VBLANK and VSYNC.
+    STA VBLANK              ; Sets the register bit value for VBLANK to ON.
+
+
+; Fall into FrameStart from the Initialize subroutine.
+; This subroutine controls setting the VSYNC and VBLANK and handles the
+; three mandatory scanlines required for VSYNC. 
+FrameStart
+    STA VSYNC               ; Sets the register bit value for VSYNC to ON.
+                            ; #2 guaranteed to be in register at this point from the Initialize and OverscanPeriod sections.
+                            ; VBLANK is set to ON in the Initialize and OverscanPeriod sections.
+
+    ; Outputs the three lines of the VSYNC signal required by the NTSC standard.
+    STA WSYNC               ; Strobe the WSYNC register, i.e. signal the CPU to wait for the current
+                            ; scanline to finish printing to screen before continuing operations.
+    STA WSYNC               ; Strobe.
+    STA WSYNC               ; Strobe.
+    LDA #0                  ; Put zero in accumulator so VSYNC can be turned off.
+    STA VSYNC               ; Turn off VYSNC.
+    ; We now fall into the vertical blank period.
+
+; Handles the 37 lines of vertical blank. 76 CPU clock cycles per VBLANK period.
+VBlankPeriod
+    LDX #37                 ; Store the value 37 into X to decrement down for each line of VBLANK.
+                            ; Counting down is better because you save an instruction by being able
+                            ; to use the zero bit in the processor status register instead of having
+                            ; to do a CMP operation.
+
+VBlankLoop
+    STA WSYNC               ; Strobe.
+    DEX                     ; Decrement the X register containing our line counter.
+    BNE VBlankLoop          ; Loop until VBLANK is complete (i.e. until X == 0).
+
+    ; Turn off VBLANK.
+    STX VBLANK              ; Stores X into VBLANK since we know X is zero at this point in the code.
+
+    ; Now fall into the portion with HBLANKS and visible output to TV.
+
+; Handles the 192 scanlines of visible frame, as well as the intervening
+; horizontal blanks. 22 CPU cycles of HBLANK followed by 54 cycles of
+; drawing to the screen
+VisibleFrame
+    LDX #192                ; Have X be the line counter for the 192 scanlines of visible frame.
+
+; Set registers for the TIA within 22 CPU cycles of HBLANK before it starts printing to the screen.
+HBlankPeriod
+
+; Do logic operations within 54 CPU cycles while the TIA is printing the image to the screen.
+PrintingPeriod
+    STA WSYNC               ; Strobe.
+    DEX                     ; Decrement our line counter.
+    BNE HBlankPeriod        ; Wait for the printing period to be done, then start again.
+
+; Handles the 30 lines of overscan, 76 clock cycles each.
+OverscanPeriod
+    LDA #2                  ; Put 2 into accumulator again in order to switch on VBLANK.
+    STA VBLANK              ; Turn on register indicating vertical blank.
+    LDX #30                 ; Make X the line counter for the 30 lines of overscan.
+
+OverscanLoop
+    STA WSYNC               ; Strobe.
+    DEX                     ; Decrement the line counter.
+    BNE OverscanLoop        ; Repeat until X = 0.
+
+    JMP FrameStart          ; When the overscan period is complete, start the next frame.
+
+; Sets the ROM's capacity to exactly 4K, i.e. $F000 through $FFFF, and tells it where to start execution.
+    org $FFFC               ; Set origin as $FFFC, the reset pointer, to tell where to begin execution.
+    .word Initialize        ; Reset pointer at $FFFC, set to Initialize.
+    .word Initialize        ; Interrupt pointer at $FFFE (unused but apparently a good idea to set anyway).
 
 ; EOF
